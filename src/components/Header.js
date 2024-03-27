1e21a233fbb65ee24d3494946eb8a26d4beafb80
yoyo1025 <hsjejxys@gmail.com>
2024-03-27 21:57:05 +0900

    react掲示板 station2 ヘッダーの実装

---
+++ src/components/Header.js
@@ -0,0 +1,8 @@
+export const Header = () => {
+    return (
+        <header>
+            <label className="l1">掲示板</label>
+            <label className="l2"><a href="https://react.dev/">スレッドをたてる</a></label>
+        </header>
+    )
+}
---
+++ src/components/Top.js
@@ -0,0 +1,5 @@
+export const Top = () => {
+    return (
+        <h2>新着スレッド</h2>
+    )
+}
