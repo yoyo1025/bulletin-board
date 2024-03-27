1e78da8f1adef568beb29f8cfe92e543606ac26e
yoyo1025 <hsjejxys@gmail.com>
2024-03-27 23:06:40 +0900

    react掲示板 station2 axiosを用いた新着スレッドの取得・表示

--- src/components/Top.js
+++ src/components/Top.js
@@ -1,47 +1,24 @@
 import { useState, useEffect } from "react";
+import axios from 'axios';
 
 export const Top = () => {
+    const [threadList, setThreadList] = useState([{id: "", title: ""}]);
 
-    // const [threadList, setThreadList] = useState([{id: "", title: ""}]) ;
-
-    // useEffect(
-    //     () => {
-    //         fetch("railway.bulletinboard.techtrain.dev/threads")
-    //             .then(res => res.json())
-    //             .then(
-    //                 (result) => {
-    //                     setThreadList(Object.keys(result.id));
-    //                     setThreadList(Object.values(result.title));
-    //                 }
-    //             )
-    //     },[]);
-
-    const threadList = [{
-        id: 1, title: "推しについて語るスレ"
-    },{
-        id: 2, title: "今期覇権アニメ"
-    },{
-        id: 3, title: "TechTrainってどうなの？"
-    },{
-        id: 4, title: "暇な人雑談しませんか"
-    },{
-        id: 5, title: "Rustについて語るスレ"
-    },{
-        id: 6, title: "自宅警備員だけどなんか質問ある"
-    },{
-        id: 7, title: "大阪でおすすめのラーメン屋教えて"
-    }]
+    useEffect(() => {
+        axios.get('https://railway.bulletinboard.techtrain.dev/threads')
+            .then(res => {
+                setThreadList(res.data);
+            })
+    }, []);
 
     return (
         <>
             <h2>新着スレッド</h2>
-            {threadList.map((thread) => {
-            return (
-                <ul>
-                    <li key={thread.id}>{thread.title}</li>
+            {threadList.map((thread) => (
+                <ul key={thread.id}>
+                    <li>{thread.title}</li>
                 </ul>
-            )
-            })}
+            ))}
         </>
-    )
+    );
 }
