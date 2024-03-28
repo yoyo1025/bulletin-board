33994528c2ae6a68aeca150066d0727d30bd6658
yoyo1025 <hsjejxys@gmail.com>
2024-03-28 12:39:37 +0900

    react掲示板 staion3 スレッド新規作成の処理を追加

--- src/components/CreateNewThread.js
+++ src/components/CreateNewThread.js
@@ -1,18 +1,35 @@
 import { Header } from "./Header";
+import { useState, useEffect } from "react";
+import axios from 'axios';
 import "../App.css";
 
 export const CreateNewThread = () => {
+    const [newThreadTitle, setNewThreadTitle] = useState("");
+
+    const sendNewThread = (event) => {
+        event.preventDefault(); 
+        axios.post('https://railway.bulletinboard.techtrain.dev/threads', { title: newThreadTitle })
+        .then(response => console.log(response.data))
+        .then(setNewThreadTitle(newThreadTitle === ""))
+        .catch(error => console.error(error));
+    }
+    
     return (
         <>
             <Header />
             <h2>スレッドの新規作成</h2>
-            <form className="create-new-thread">
+            <form className="create-new-thread" onSubmit={sendNewThread}>
                 <div className="thread-input-container">
-                    <input type="text" placeholder="スレッドタイトル" />
+                    <input 
+                        value={newThreadTitle} 
+                        type="text" 
+                        placeholder="スレッドタイトル" 
+                        onChange={e => setNewThreadTitle(e.target.value)}
+                    />
                     <button type="submit">作成</button>
                 </div>
                 <a href="/">Topに戻る</a>
             </form>
         </>
-    )
+    );
 }
